1921f57f1b98a3957fe9d1467f0003fce8eec250
Tom Haelbich <haelbito@outlook.com>
2026-03-13 22:10:17 +0100

    Update versionCode and remove unused permissions
    Incremented android:versionCode to 4 in AndroidManifest.xml. Removed ReadMediaAudio and ReadMediaVideo permissions from MainApplication.cs to streamline app permissions and reduce unnecessary access.

--- PixelsorterApp/Platforms/Android/MainApplication.cs
+++ PixelsorterApp/Platforms/Android/MainApplication.cs
@@ -1,9 +1,7 @@
 ﻿using Android.App;
 using Android.Runtime;
 [assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage, MaxSdkVersion = 32)]
-[assembly: UsesPermission(Android.Manifest.Permission.ReadMediaAudio)]
 [assembly: UsesPermission(Android.Manifest.Permission.ReadMediaImages)]
-[assembly: UsesPermission(Android.Manifest.Permission.ReadMediaVideo)]
 [assembly: UsesPermission(Android.Manifest.Permission.AccessNetworkState)]
 namespace PixelsorterApp
 {
